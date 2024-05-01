2fb5e6ad1727b070ee54beeff32168f667adb3a2
Auriol Degbelo <auriol.degbelo@tu-dresden.de>
2024-05-01 10:59:11 +0200

    event listerners

--- LE02/events.js
+++ LE02/events.js
@@ -4,8 +4,8 @@
 
 let element = document.getElementById("testparagraph")
 
+
 /*
-
 element.onclick = function()
 {
     alert ("I have been clicked upon")
@@ -16,6 +16,8 @@ element.onclick = function()
     alert ("Second event handler")
 }
 
+
+
 element.addEventListener("click", function()
 {
     alert ("I have been clicked upon")
@@ -25,4 +27,26 @@ element.addEventListener("click", function()
 {
     alert ("Second event handler")
 })
+
+
+element.addEventListener("mouseover", function(){
+
+    alert("Mouseover")
+})
+
+
+element.addEventListener("click", firstHandler)
+element.addEventListener("click", secondHandler)
+
+
+function firstHandler()
+{
+    alert ("I have been clicked upon")
+}
+
+function secondHandler()
+{
+    alert ("I am the second handler")
+}
+
 */
